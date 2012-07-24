d30afd23cdb976c57d0a52c231d49acb88293a04
David Winterbottom <david.winterbottom@gmail.com>
2012-07-24 10:08:57 +0100

    Updated RSS feed to show longer description

--- www/rsb/views.py
+++ www/rsb/views.py
@@ -1,13 +1,13 @@
-from django.views.generic import TemplateView, DetailView, ListView, RedirectView
-from django.core.urlresolvers import reverse
 from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, DetailView, ListView, RedirectView
+from django.utils import text
 from tagging.models import Tag
-import requests
 
+from rsb.github import fetch_activity
 from rsb.models import Article
 from rsb.twitter import fetch_tweets
-from rsb.github import fetch_activity
 
 
 class HomeView(TemplateView):
@@ -17,15 +17,15 @@ class HomeView(TemplateView):
         return {'articles': Article.objects.all().order_by('-date_published')[:5],
                 'tweets': fetch_tweets(),
                 'github_activity': fetch_activity()[:8]}
-    
-        
+
+
 class ArticleListView(ListView):
     model = Article
     template_name = 'rsb/article_list.html'
     context_object_name = 'articles'
     title = "All articles"
     popular = False
-    
+
     def get_queryset(self):
         qs = self.model.objects.all().exclude(date_published=None)
         if self.popular:
@@ -34,7 +34,7 @@ class ArticleListView(ListView):
         else:
             qs = qs.order_by('-date_published')
         return qs
-    
+
     def get_context_data(self, **kwargs):
         ctx = super(ArticleListView, self).get_context_data(**kwargs)
         ctx['title'] = self.title
@@ -55,16 +55,16 @@ class ArticlesFeedView(Feed):
 
     def item_description(self, item):
         return item.summary
-    
+
 
 class ArticleTagView(ListView):
     template_name = 'rsb/article_list.html'
     context_object_name = 'articles'
-    
+
     def get_queryset(self):
         self.tag = get_object_or_404(Tag, name=self.kwargs['name'])
         return Article.tagged.with_all([self.tag])
-    
+
     def get_context_data(self, **kwargs):
         ctx = super(ArticleTagView, self).get_context_data(**kwargs)
         ctx['title'] = self.tag.name.title()
@@ -78,29 +78,32 @@ class ArticleTagFeed(Feed):
         return get_object_or_404(Tag, name=name)
 
     def title(self, obj):
-        return 'Writing on %s' % obj.name
-    
+        return 'Writing on %s | David Winterbottom' % obj.name
+
     def link(self, obj):
         return reverse('tagged-feed', kwargs={'name': obj.name})
 
     def items(self, obj):
         return Article.tagged.with_all([obj])
 
+    def item_description(self, item):
+        return text.truncate_html_words(item.body_html, 120)
+
 
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'rsb/article_detail.html'
     context_object_name = 'article'
-    
+
     def get(self, request, **kwargs):
         response = super(ArticleDetailView, self).get(request, **kwargs)
         # Track the view
         self.object.record_view()
         return response
-    
+
     def get_context_data(self, **kwargs):
         ctx = super(ArticleDetailView, self).get_context_data(**kwargs)
-        
+
         article = self.object
         ctx['related_articles'] = Article.tagged.related_to(article)[:6]
 
@@ -108,7 +111,7 @@ class ArticleDetailView(DetailView):
         ids_to_ignore = [a.id for a in ctx['related_articles']]
         ids_to_ignore.append(article.id)
         ctx['popular_articles'] = Article.objects.all().exclude(id__in=ids_to_ignore).order_by('-num_views')[:6]
-        
+
         # We need to use a different date field for comparison depending on
         # if the article is published
         if article.is_published:
@@ -117,18 +120,18 @@ class ArticleDetailView(DetailView):
         else:
             previous_articles = Article.objects.filter(date_created__lt=article.date_created)
             next_articles = Article.objects.filter(date_created__gt=article.date_created)
-        
+
         ctx['previous_article'] = previous_articles[0] if len(previous_articles) > 0 else None
         ctx['next_article'] = next_articles[0] if len(next_articles) > 0 else None
 
         return ctx
-    
-    
+
+
 class ArticleRedirectView(RedirectView):
     """
     For ensuring SEO goodness for URLs from the old site
     """
-    
+
     def get_redirect_url(self, **kwargs):
         try:
             article = Article.objects.get(old_id=kwargs['id'])
@@ -136,7 +139,7 @@ class ArticleRedirectView(RedirectView):
             return reverse('home')
         else:
             return article.get_absolute_url()
-    
-    
+
+
 class AboutView(TemplateView):
     template_name = 'rsb/about.html'
--- www/urls.py
+++ www/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls.defaults import *
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.conf.urls.defaults import patterns, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,9 +9,10 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'', include('rsb.urls')),
 )
+
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += patterns('',
-            (r'^500.html', 'django.views.generic.simple.direct_to_template', {'template': '500.html'}),
-            (r'^404.html', 'django.views.generic.simple.direct_to_template', {'template': '404.html'}),
-        )
+        (r'^500.html', 'django.views.generic.simple.direct_to_template', {'template': '500.html'}),
+        (r'^404.html', 'django.views.generic.simple.direct_to_template', {'template': '404.html'}),
+    )
