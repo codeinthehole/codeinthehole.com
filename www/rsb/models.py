0d34c7acda0d071f2be7f8c14c31dfdac1dbc289
David Winterbottom <david.winterbottom@gmail.com>
2013-04-18 10:07:21 +0100

    Add warning on old articles

--- www/rsb/models.py
+++ www/rsb/models.py
@@ -1,4 +1,6 @@
 import tagging
+import datetime
+import math
 
 from django.db import models
 from django.template.defaultfilters import slugify
@@ -9,14 +11,14 @@ class Article(models.Model):
     """
     Blog article
     """
-    
+
     # This is used to identify the article
     filename = models.CharField(max_length=255, unique=True)
 
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     summary = models.TextField()
-    
+
     # Article is created in RST, oonverted to HTML
     body_rst = models.TextField()
     body_html = models.TextField()
@@ -25,12 +27,12 @@ class Article(models.Model):
     num_views = models.PositiveIntegerField(default=0, db_index=True)
 
     date_created = models.DateTimeField(auto_now_add=True)
-    
-    # This is the date the article was uploaded from the dev 
+
+    # This is the date the article was uploaded from the dev
     # environment to production.  That is what "published" means.
     date_published = models.DateTimeField(null=True, blank=True)
     date_updated = models.DateTimeField(null=True, blank=True)
-    
+
     # The ID from the old blogging software to allow redirects
     old_id = models.IntegerField(null=True, blank=True)
 
@@ -51,10 +53,23 @@ class Article(models.Model):
     @property
     def is_published(self):
         return self.date_published is not None
-    
+
+    @property
+    def age(self):
+        now = datetime.datetime.now()
+        return now - self.date_published
+
+    @property
+    def age_in_years(self):
+        return int(math.floor(self.age.days / 365))
+
+    @property
+    def is_old(self):
+        return self.age_in_years >= 2
+
     def record_view(self):
         self.num_views += 1
         self.save()
-    
-    
+
+
 tagging.register(Article)
