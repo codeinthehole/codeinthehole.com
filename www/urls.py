ecf68d010e11d9b735e8d75d5b1c6e242b39e00a
David Winterbottom <david.winterbottom@gmail.com>
2014-06-16 20:10:34 +0100

    Add new page to main nav

--- www/urls.py
+++ www/urls.py
@@ -1,23 +1,23 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.views import generic
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^admin/', include(admin.site.urls)),
-    (r'^cv/', generic.TemplateView.as_view(
-        template_name='cv.html')),
+    url(r'^cv/', generic.TemplateView.as_view(
+        template_name='cv.html'), name="cv"),
     (r'', include('rsb.urls')),
 )
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
-
-    from django.views import generic
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         (r'^404.html', generic.TemplateView.as_view(template_name='404.html')),
         (r'^500.html', generic.TemplateView.as_view(template_name='500.html')),
     )
